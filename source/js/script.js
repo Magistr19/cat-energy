8de003d39c682b7703f83be679febb96debdb742
Alex <sashapayareli@gmail.com>
2018-11-05 00:49:29 +0200

    СдеJS мобильная навигация

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,20 @@
-﻿
+﻿/* Js active */
+
+var bodyElement = document.querySelector ('.no-js');
+bodyElement.classList.remove('no-js');
+
+/* Mobile nav open */
+
+var toogleBtn = document.querySelector ('.toggle-nav');
+var mobileNav = document.querySelector ('.main-nav');
+
+toogleBtn.addEventListener("click", function(evt) {
+  if (toogleBtn.classList.contains('toggle-nav--open')) {
+    toogleBtn.classList.remove ('toggle-nav--open');
+    mobileNav.classList.remove ('main-nav--open');
+  }
+  else {
+    toogleBtn.classList.add ('toggle-nav--open');
+    mobileNav.classList.add ('main-nav--open');
+  }
+});
